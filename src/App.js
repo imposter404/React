dd8eeba379ed38493588656898f86216f878aab1
Imposter <ara404.github@gmail.com>
2025-08-29 13:09:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+function App(){
+  return<div className="home">
+    <div className="grid-container">
+      <div className="box red">
+        <div className="container">
+          div
+        </div>
+      </div>
+      <div className="box red"></div>
+      <div className="box red"></div>
+    </div>
+  </div>
+}
+
+
+
+
+
+
+export default App;
---
+++ src/Components/in.jsx
@@ -0,0 +1,10 @@
+import './in.css';
+
+
+function In(){
+
+    return <div>HI</div>
+    
+}
+
+export default In
